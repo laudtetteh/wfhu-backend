f0a1c7a760030cc7f2fd3f5e78a92eb989170ada
Laud Tetteh <laud@studiotenfour.com>
2021-01-26 13:04:02 +0000

    Posts model: change  image collection to single  of type file

--- api/page/controllers/Page.js
+++ api/page/controllers/Page.js
@@ -8,14 +8,6 @@ module.exports = {
       pages = await strapi.api.page.services.page.find(ctx.query);
     }
 
-    // pages = await Promise.all(
-    //   pages.map(async (page) => {
-    //     page.note = await strapi.api.review.services.review.average(page.id);
-
-    //     return page;
-    //   })
-    // );
-
     return pages;
   },
 
@@ -26,41 +18,6 @@ module.exports = {
     if (!page) {
       return ctx.notFound();
     }
-
-    // page.note = await strapi.api.review.services.review.average(page.id);
-
-    // let noteDetails = await strapi
-    //   .query('review')
-    //   .model.query(function (qb) {
-    //     qb.where('page', '=', page.id);
-    //     qb.groupBy('note');
-    //     qb.select('note');
-    //     qb.count();
-    //   })
-    //   .fetchAll()
-    //   .then((res) => res.toJSON());
-
-    // page.noteDetails = [];
-
-    // for (let i = 5; i > 0; i--) {
-    //   let detail = noteDetails.find((detail) => {
-    //     return detail.note === i;
-    //   });
-
-    //   if (detail) {
-    //     detail = {
-    //       note: detail.note,
-    //       count: detail['count(*)'],
-    //     };
-    //   } else {
-    //     detail = {
-    //       note: i,
-    //       count: 0,
-    //     };
-    //   }
-
-    //   page.noteDetails.push(detail);
-    // }
 
     return page;
   }
--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -1,1 +1,24 @@
-module.exports = {};
+module.exports = {
+  find: async (ctx) => {
+    let posts;
+
+    if (ctx.query._q) {
+      posts = await strapi.api.post.services.post.search(ctx.query);
+    } else {
+      posts = await strapi.api.post.services.post.find(ctx.query);
+    }
+
+    return posts;
+  },
+
+  findOne: async (ctx) => {
+    const { id } = ctx.params;
+    let post = await strapi.api.post.services.post.findOne({ id });
+
+    if (!post) {
+      return ctx.notFound();
+    }
+
+    return post;
+  }
+};
