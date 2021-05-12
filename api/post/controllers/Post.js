177aa43dbeb8b80525c12c24452a077db643e632
Laud Tetteh <laud@studiotenfour.com>
2021-05-12 19:07:41 +0000

    Cleanup on Post content type contoller, etc.

--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -5,52 +5,22 @@ module.exports = {
         let entities;
 
         if (ctx.query._q) {
-            entities = await strapi.api.post.services.post.search(ctx.query);
+            entities = await strapi.services.post.search(ctx.query);
         } else {
-            entities = await strapi.api.post.services.post.find(ctx.query);
+            entities = await strapi.services.post.find(ctx.query);
         }
 
         return entities;
-
-        // return entities.map(entity => {
-        //     const post = sanitizeEntity(entity, {
-        //         model: strapi.models.post,
-        //     });
-
-        //     if( post.previous_ ) {
-        //         delete post.previous_;
-        //     }
-
-        //     if( post.category && post.category.previous_) {
-        //         delete post.category.previous_;
-        //     }
-
-        //     return post;
-        // });
     },
 
     findOne: async (ctx) => {
         const { id } = ctx.params;
-        let entity = await strapi.api.post.services.post.findOne({ id });
+        let entity = await strapi.services.post.findOne({ id });
 
         if (!entity) {
             return ctx.notFound();
         }
 
         return entity;
-        
-        // const post = sanitizeEntity(entity, {
-        //     model: strapi.models.post,
-        // });
-
-        // if( post.category && post.category.previous_) {
-        //     delete post.category.previous_;
-        // }
-
-        // if(post.previous_ ) {
-        //     delete post.previous_;
-        // }
-
-        return post;
     }
 };
